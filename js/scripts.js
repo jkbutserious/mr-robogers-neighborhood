30af85928b0eaacc9364e2fba22fc9dae567bbc0
Jeff Kim <kim2jy@hotmail.com>
2020-06-12 10:32:41 -0700

    Actually fulfill spec 1 - correct misunderstanding about returned number sequence

--- js/scripts.js
+++ js/scripts.js
@@ -3,18 +3,22 @@ $(document).ready(function() {
   $("#formOne").submit(function(event) {
     event.preventDefault();
 
-    const userNumber = $("input#user-number").val();
+    const userNumber = parseInt($("input#user-number").val());
 
     $("#robo-response").append(roboResponse(userNumber));
-  })
-})
+  });
+});
 
 // Business Logic
 const roboResponse = function(number) {
-  const numberArray = number.split("");
   let numberSequence = "";
+  const numberArray = [];
 
-  for (i = 0; i < numberArray.length; i++) {
+  for (i = 0; i < number; i++) {
+    // if (numberArray[i] === 1) {
+    //   numberArray[i] = "Beep!";
+    // }
+    numberArray[i] = i;
     numberSequence = numberSequence + " " + numberArray[i];
   }
   return numberSequence;
