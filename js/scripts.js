7c7b630a303a6a710e4e3bad62516314bc8b1b8b
Jeff Kim <kim2jy@hotmail.com>
2020-06-12 14:23:00 -0700

    Add the ability for Mr Robogers to ask users to be his neighbor by name for numbers divisible by 3 and add a text field for users to input their names

--- js/scripts.js
+++ js/scripts.js
@@ -3,20 +3,24 @@ $(document).ready(function() {
   $("#formOne").submit(function(event) {
     event.preventDefault();
     const userNumber = parseInt($("input#user-number").val());
+    const userName = $("input#user-name").val();
     $("#output").show();
     $("#robo-response").text("");
-    $("#robo-response").append(roboResponse(userNumber));
+    $("#robo-response").append(roboResponse(userNumber, userName));
   });
 });
 
 // Business Logic
-const roboResponse = function(number) {
+const roboResponse = function(number, name) {
   let numberSequence = "";
   const numberArray = [];
 
   for (i = 0; i <= number; i++) {
     numberArray[i] = i;
-    if ((numberArray[i]).toString().match(/3/) != null) {
+    if (numberArray[i] % 3 === 0 && numberArray[i] != 0) {
+      numberArray[i] = "Won't you be my neighbor, " + name + "?";
+      }
+    else if ((numberArray[i]).toString().match(/3/) != null) {
       numberArray[i] = "Won't you be my neighbor?";
       }
     else if ((numberArray[i]).toString().match(/2/) != null) {
